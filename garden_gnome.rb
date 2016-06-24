ee7a0b75ab36ffc189057abe7bfccea9ef842c9c
Savannah Wigger <savannahwigger@yahoo.com>
2016-06-24 18:28:24 +0000

    We finished the gnome lab

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,24 @@
-# Code your instances here
+# Code your class GardenGnome
+class GardenGnome
+  attr_accessor :name, :age, :gluten_allergy, :hat_color
+
+  attr_reader :personality
+
+  def initialize(hat_color="red")
+    @personality = "evil"
+    @hat_color = hat_color
+  end
+
+  def gnaw
+    return "Gnawing on a tree!!!"
+end
+
+  def shout
+    return "GNARLY!!!"
+  end
+
+  def introduce_self
+    return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+end
+
+end
